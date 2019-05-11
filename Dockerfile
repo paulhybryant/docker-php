6ef838863ad98dc5f7dc6818c164fc5ff6d98713
paulhybryant <paulhybryant@gmail.com>
2019-05-11 15:08:07 +0800

    .

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+ARG ARCH=docker.io
+FROM ${ARCH}/php:apache
+
+LABEL maintainer="paulhybryant@gmail.com"
+
+COPY qemu-aarch64-static /usr/bin/
+
+Add php/* /var/www/html
---
+++ php/nas.php
@@ -0,0 +1,11 @@
+<?php
+  $file = fopen("nas.txt","w");
+  echo fwrite($file, $_SERVER['REMOTE_ADDR']);
+  fclose($file);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<body>
+<h1 style="color: #5e9ca0;" data-darkreader-inline-color="">Your IP: <?php echo $_SERVER['REMOTE_ADDR'?></h1>
+</body>
+</html>
