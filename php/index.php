6584d38a64327713ff6f8605aa99bc691ae965a6
paulhybryant <paulhybryant@gmail.com>
2019-05-13 15:03:19 +0800

    .

--- php/index.php
+++ php/index.php
@@ -8,7 +8,7 @@
     $port = 8081;
   } else {
     $protocol  = "http";
-    $port = 8080:
+    $port = 8080;
   }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
