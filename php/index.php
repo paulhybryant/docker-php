04bd84029ae3f2a524ceb0762c609b3847bb1930
paulhybryant <paulhybryant@gmail.com>
2019-05-13 14:58:04 +0800

    .

--- php/index.php
+++ php/index.php
@@ -2,6 +2,14 @@
   $myfile = fopen("nas.txt", "r") or die("Unable to open file!");
   $ip = fread($myfile,filesize("nas.txt"));
   fclose($myfile);
+
+  if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') {
+    $protocol  = "https";
+    $port = 8081;
+  } else {
+    $protocol  = "http";
+    $port = 8080:
+  }
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -12,7 +20,7 @@
 <?php
   $apps = array("admin.php", "calibre", "kod", "aria2");
   foreach ($apps as $app) {
-    echo '<li style="color: #2e6c80;" data-darkreader-inline-color=""><a href="http://' . $ip . ':8081/' . $app . '">' . $app . '</a></li>';
+    echo '<li style="color: #2e6c80;" data-darkreader-inline-color=""><a href="' . $protocol . '://' . $ip . ':' . $port . '/' . $app . '">' . $app . '</a></li>';
   }
 ?>
 </ul>
